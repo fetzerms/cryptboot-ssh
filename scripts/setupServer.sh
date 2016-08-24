bbf231b45b2b3d7a434dfc773573eee6b381b87e
privat <privat@privat-pc.lan>
2016-08-24 20:57:39 +0200

    Renamed and fixed small bug in setupServer.sh

--- scripts/setupServer.sh
+++ scripts/setupServer.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-clientName=$1
-if [ -z $clientName ] ; then
-        echo "Usage: $0 clientName"
+if [ "$#" -ne 2 ] ; then
+        echo "Usage: $0 <clientName> <clientMac>"
         exit 0
 fi
-mkdir ~/crypt-{keys,scripts}
-mkdir ~/.ssh
+
+clientName=$1
+clientMac=$2
+
+# set up dirs.
+mkdir -p ~/crypt-{keys,scripts}
+mkdir -p ~/.ssh
 
 # Generate a random keyfile for $clientName, but only if it doesn't exist yet.
 if [ ! -f ~/crypt-keys/"$clientName".keyfile ] ; then
 	dd bs=512 count=4 if=/dev/urandom of=~/crypt-keys/"$clientName".keyfile iflag=fullblock 
 fi
+
 # Download the server-keyscript and make it executable
 cp server/retrieve_crypto_key ~/crypt-scripts/retrieve_"$clientName"_key
 chmod +x ~/crypt-scripts/retrieve_"$clientName"_key
